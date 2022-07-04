fd2437499aabb11a37dc8f8b534daf654fa6c7de
Gimaltdinov <96269132+RenatGim@users.noreply.github.com>
2022-07-04 22:26:21 +0700

    Add files via upload

---
+++ WepApp2.2/Controllers/CalcController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp2._2.Models;
+
+
+namespace WebApp2._2.Controllers
+{
+    public class CalcController : Controller
+    {
+        private Random _random { get; set; } = new Random();
+
+        private readonly ILogger<CalcController> _logger;
+
+        public CalcController(ILogger<CalcController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("Manual")]
+        public IActionResult ManualParsingSingle()
+        {
+            if (Request.Method == "POST")
+            {
+                try
+                {
+                    var calc = new CalculatorViewModel()
+                    {
+                        X = Int32.Parse(HttpContext.Request.Form["x"]),
+                        Action = HttpContext.Request.Form["action"],
+                        Y = Int32.Parse(HttpContext.Request.Form["y"])
+                    };
+                    ViewBag.Result = calc.Calc();
+                }
+                catch
+                {
+                    ViewBag.Result = "Invalid input";
+                }
+                return View("Result");
+            }
+            return View("Manual");
+        }
+
+        //-----------------------------------------
+
+        [HttpGet]
+        [ActionName("ManualWithParsingSeparate")]
+        public IActionResult ManualWithParsingSeparateGet()
+        {
+            return View("Manual");
+        }
+        [HttpPost]
+        [ActionName("ManualWithParsingSeparate")]
+        public IActionResult ManualWithParsingSeparatePost()
+        {
+            try
+            {
+                var calc = new CalculatorViewModel()
+                {
+                    X = Int32.Parse(HttpContext.Request.Form["x"]),
+                    Action = HttpContext.Request.Form["action"],
+                    Y = Int32.Parse(HttpContext.Request.Form["y"])
+                };
+                ViewBag.Result = calc.Calc();
+            }
+            catch
+            {
+                ViewBag.Result = "Invalid input";
+            }
+            return View("Result");
+        }
+
+        //-----------------------------------------
+
+        [HttpGet]
+        [ActionName("ModelBindingInParameters")]
+        public IActionResult ModelBindingParametersGet()
+        {
+            return View("Manual");
+        }
+
+        [HttpPost]
+        [ActionName("ModelBindingInParameters")]
+        public IActionResult ModelBindingParametersPost(int x, string action, int y)
+        {
+            if (ModelState.IsValid)
+            {
+                var calc = new CalculatorViewModel
+                {
+                    X = x,
+                    Y = y,
+                    Action = action
+                };
+                ViewBag.Result = calc.Calc();
+            }
+            else
+            {
+                ViewBag.Result = "Invalid input";
+            }
+            return View("Result");
+        }
+
+        //-----------------------------------------
+
+        [HttpGet]
+        [ActionName("ModelBindingInSeparateModels")]
+        public IActionResult ModelBindingSeparateGet()
+        {
+            return View("Manual");
+        }
+
+        [HttpPost]
+        [ActionName("ModelBindingInSeparateModels")]
+        public IActionResult ModelBindingSeparatePost(CalculatorViewModel model)
+        {
+            ViewBag.Result = ModelState.IsValid
+                ? model.Calc()
+                : "Invalid input";
+
+            return View("Result");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WepApp2.2/Models/CalculatorViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApp2._2.Models
+{
+    public class CalculatorViewModel
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Action { get; set; }
+        public string Calc() 
+        {
+            return Action switch
+            {
+                "+" => $"{X} + {Y} = {X + Y}",
+                "-" => $"{X} - {Y} = {X - Y}",
+                "*" => $"{X} * {Y} = {X * Y}",
+                "/" when Y != 0 => $"{X} / {Y} = {X / Y}",
+                "/" when Y == 0 => "Error: division by zero",
+                _ => "Invalid operation"
+            };
+        }
+    }
+}
